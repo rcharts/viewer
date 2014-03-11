12fd2ba5e32c57f2dc0ab6737d8c7e035572deb3
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2014-03-10 22:22:51 -0400

    Update pagist.js

--- pagist.js
+++ pagist.js
@@ -38,8 +38,6 @@ Pagist.DEFAULT_LAYOUT = function(html) {
     +  this.ribbon 
     + '<h2>' + (this.title || '') + this.editme + '</h2>'
     +   this.sharing
-    +  '<br/>'
-    +   this.editme
     +   html
     +   this.editme
     +   this.comments
@@ -124,7 +122,7 @@ Pagist.main = function() {
      ribbon = '<a href="' + res.data.html_url + '">' + 
      '<img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_green_007200.png" alt="Fork me on GitHub"></a>',
      comments = " <div id=\"disqus_thread\"></div>\n    <script type=\"text/javascript\">\n        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */\n        var disqus_shortname = 'rcharts'; // required: replace example with your forum shortname\n\n        /* * * DON'T EDIT BELOW THIS LINE * * */\n        (function() {\n            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;\n            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';\n            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);\n        })();\n    </script>\n    <noscript>Please enable JavaScript to view the <a href=\"http://disqus.com/?ref_noscript\">comments powered by Disqus.</a></noscript>\n    <a href=\"http://disqus.com\" class=\"dsq-brlink\">comments powered by <span class=\"logo-disqus\">Disqus</span></a>\n    ",
-     sharing = '<!-- AddThis Button BEGIN -->\n<div class="addthis_toolbox addthis_default_style ">\n<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>\n<a class="addthis_button_tweet"></a>\n<a class="addthis_button_pinterest_pinit"></a>\n<a class="addthis_counter addthis_pill_style"></a>\n' + editme + '</div>\n<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>\n<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4fdfcfd4773d48d3"></script>\n<!-- AddThis Button END -->'
+     sharing = '<!-- AddThis Button BEGIN -->\n<div class="addthis_toolbox addthis_default_style ">\n<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>\n<a class="addthis_button_tweet"></a>\n<a class="addthis_button_pinterest_pinit"></a>\n<a class="addthis_counter addthis_pill_style"></a>\n' + '</div>\n<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>\n<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4fdfcfd4773d48d3"></script>\n<!-- AddThis Button END -->'
      context = { footer: footer, title: title, ribbon: ribbon, comments: comments,
        sharing: sharing, editme: editme }
     document.write(Pagist.render(list, context))
